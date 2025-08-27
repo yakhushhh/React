cc00a4fa401cda94731c1220d512352af29842cb
Khushi Yadav <yadavkhushistr@gmail.com>
2025-08-27 20:46:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+const App = () => {
+  return (
+  <div> 
+    <nav>
+      <div className="logo">
+        <img src = "C:\Users\hp\OneDrive\Desktop\all\project++\react-Learning\frontend practices\react01\day01react\public\45175a94d5389cc06b9d40c646878c60ac4bf7ed.png" alt="logo"/>
+        </div>
+        <ul>
+          <li href= "#">Menu</li>
+          <li href= "#">Location</li>
+          <li href= "#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+      <button>Login</button>
+    </nav>
+  </div>
+  );
+  
+
+
+};
+export default App;
+ 
