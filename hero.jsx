6a8b7fbbe35772bcf18007ade4d2d727316024ef
Khushi Yadav <yadavkhushistr@gmail.com>
2025-08-30 02:32:03 +0530

    Add files via upload

---
+++ hero.jsx
@@ -0,0 +1,34 @@
+const herosection= ()=>{
+  return (
+  <main className="hero">
+    <div className="hero-content">
+      <h1>YOUR FEET DESERVE THE BEST</h1>
+      <P>
+        Your FEER DESERVE THE BEST AND WE'RE HERE TO HELP TO YOU WITH OUR SHOES. YOUR FEET DESREVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR 
+        SHOES.
+      </P>
+      <div className="hero-button">
+        <button>Shop Now</button>
+        <button>Category</button>
+      </div>
+      <div className="Shopping">
+        <p>Also Available On</p>
+
+      </div>
+      < className="brand-icon">
+        <img src="src/flipkart.png" alt="Flipkart"></img>
+        <img scr="src/amazon.png" alt= "Amazon">
+        </img>
+      </div>
+
+
+
+    </div>
+    <div className="hero"
+    <div className="hero-image">
+
+    </div>
+  </main>
+  );
+};
+export default herosection;
